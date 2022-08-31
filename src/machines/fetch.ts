6450c213f1445deacf6ed5e58058e349adbc95d4
Muhammad Nasrurrohman <anas@jurnalanas.com>
2022-08-31 13:26:14 +0700

    added notes

--- src/machines/fetch.ts
+++ src/machines/fetch.ts
@@ -27,6 +27,7 @@ export const fetchMachine = Machine<
   {
     id: 'fetch',
     initial: 'idle',
+    // context: to store data
     context: {
       results: [],
       message: ''
@@ -40,11 +41,13 @@ export const fetchMachine = Machine<
       pending: {
         entry: ['fetchData'],
         on: {
+          // target state in an object, so you can add `actions` to it
           RESOLVE: { target: 'successful', actions: ['setResults'] },
           REJECT: { target: 'failed', actions: ['setMessage'] }
         }
       },
       failed: {
+        // if no actions needed, can just put it in a string
         on: {
           FETCH: 'pending'
         }

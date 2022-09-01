dde344e5027d7b040890bb4103e5adf7468fc833
Muhammad Nasrurrohman <anas@jurnalanas.com>
2022-09-01 10:00:31 +0700

    added example1end

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Example1 from "./example1";
+import Example1End from "./example2";
+
 import "./App.css";
 
 export default function App() {
@@ -12,11 +14,15 @@ export default function App() {
             <li>
               <Link to="/example1">Example 1</Link>
             </li>
+            <li>
+              <Link to="/example1-end">Example 2</Link>
+            </li>
           </ul>
         </nav>
 
         <Routes>
           <Route path="/example1" element={<Example1 />} />
+          <Route path="/example1-end" element={<Example1End />} />
         </Routes>
       </div>
     </Router>
